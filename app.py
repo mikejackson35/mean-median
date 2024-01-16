7f227f8c7dc9ead36be1cd502cbab2516ba78473
mikejackson35 <mikejackson35@gmail.com>
2024-01-16 14:14:13 -0800

    initial commit

---
+++ app.py
@@ -0,0 +1,137 @@
+#######################
+# Import libraries
+import streamlit as st
+import altair as alt
+import plotly.graph_objects as go
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import seaborn as sns
+import matplotlib.pyplot as plt
+from utils import get_rec_table_skinny, get_rec_table_wide, get_player_scatter_vertical, get_player_scatter_horizontal
+
+#######################
+# Page configuration
+st.set_page_config(
+    page_title="mean-median",
+    page_icon="🏂",
+    layout="wide",
+    initial_sidebar_state="expanded")
+
+# alt.themes.enable("dark")
+
+#######################
+# CSS styling
+st.markdown("""
+<style>
+
+[data-testid="block-container"] {
+    padding-left: 2rem;
+    padding-right: 2rem;
+    padding-top: 2rem;
+    padding-bottom: 0rem;
+    margin-bottom: -7rem;
+}
+
+[data-testid="stVerticalBlock"] {
+    padding-left: 0rem;
+    padding-right: 0rem;
+}
+
+[data-testid="stMetric"] {
+    background-color: #393939;
+    text-align: center;
+    padding: 15px 0;
+}
+
+[data-testid="stMetricLabel"] {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+[data-testid="stMetricDeltaIcon-Up"] {
+    position: relative;
+    left: 38%;
+    -webkit-transform: translateX(-50%);
+    -ms-transform: translateX(-50%);
+    transform: translateX(-50%);
+}
+
+[data-testid="stMetricDeltaIcon-Down"] {
+    position: relative;
+    left: 38%;
+    -webkit-transform: translateX(-50%);
+    -ms-transform: translateX(-50%);
+    transform: translateX(-50%);
+}
+
+</style>
+""", unsafe_allow_html=True)
+
+
+#######################
+all_data = pd.read_csv(r"data\final_data.csv")
+all_data = all_data.astype({'season': int})
+
+#######################
+# Sidebar
+with st.sidebar:
+    player_list = list(all_data.player_display_name.unique())#[::-1]
+    
+    player = st.selectbox('Select a player', player_list)
+    player_all = all_data[(all_data.player_display_name==player)].reset_index(drop=True)
+    player_season = player_all[player_all.season==2023].reset_index(drop=True)
+    player_season2 = player_all[player_all.season > 2021].reset_index(drop=True)
+
+    st.caption("Underdog")
+
+    med_rec = int(player_season[player_season.book_stat == 'receiving_yards']['receiving_yards'].median())
+    med_rush = int(player_season[player_season.book_stat == 'rushing_yards']['rushing_yards'].median())
+
+    st.metric(label=f"Receiving Yards", 
+              value=f"{(player_all[player_all['book_stat']=='receiving_yards'].ud_line.mean())}", 
+              delta= med_rec)
+    st.metric(label="Rushing Yards", 
+              value=f"{player_all[player_all['book_stat']=='rushing_yards'].ud_line.mean()}", 
+              delta= med_rush)
+    st.markdown("---")
+    st.caption('Prize Picks')
+    st.metric(label=f"Receiving Yards", 
+              value=f"{player_all[player_all['book_stat']=='receiving_yards'].pp_line.mean()}", 
+              delta= med_rec)
+    st.metric(label="Rushing Yards", 
+              value=f"{player_all[player_all['book_stat']=='rushing_yards'].pp_line.mean()}", 
+              delta= med_rush)
+
+
+    
+col = st.columns((1,9))
+
+with col[0]:
+        st.markdown("")
+with col[1]:
+        st.markdown(f"<left><h1>{player_all.player_display_name[0]}</h1></left>", unsafe_allow_html=True)
+        col = st.columns((1.5,1.5,7))
+        with col[0]:
+             st.markdown(f"<left><h1 style='color:yellow'>ud{player_season[player_season.book_stat=='receiving_yards'].ud_line.mean()}</h1></left>",unsafe_allow_html=True)
+        with col[1]:
+             st.markdown(f"<left><h1 style='color:purple'>pp{player_season[player_season.book_stat=='receiving_yards'].pp_line.mean()}</h1></left>", unsafe_allow_html=True)
+
+
+    
+#######################
+# Dashboard Main Panel
+col = st.columns((2,1,1,3))
+fig = get_player_scatter_vertical(player_season)
+
+with col[0]:
+    st.plotly_chart(fig, theme=None,use_container_width=True)
+with col[1]:
+    st.markdown("##")
+    st.dataframe(get_rec_table_skinny(player_season),hide_index=True, height=700,column_config={'week':'Week','receiving_yards':'Rec Yards'},use_container_width=True)
+with col[2]:
+    st.markdown("")
+with col[3]:
+    st.markdown("##")
+    st.dataframe(get_rec_table_wide(player_season),hide_index=True, height=700, column_config={'week':'Week','targets':'Targets','receptions':'Receptions','receiving_tds':'Receiving TDs','fantasy_points':'Fantasy Points'},use_container_width=True)
---
+++ utils.py
@@ -0,0 +1,67 @@
+
+import pandas as pd
+import plotly.express as px
+
+def merge_books(pp,ud):
+    pp = pp[['Player','Position','Team','Opponent','Market Name','Line']].rename(columns={'Line':'pp_line'})
+    ud = ud[['Player','Position','Team','Opponent','Market Name','Line']].rename(columns={'Line':'ud_line'})
+    merged = pd.merge(pp,ud,on=['Player','Position', 'Team', 'Opponent','Market Name'],how='outer')
+    merged.columns = ['player_display_name','position','team','opponent','book_stat','pp_line','ud_line']
+    merged.to_csv('data/books_merged.csv', index=False)
+    return merged
+
+# --- add 'delta' column
+def pp_delta(row):
+    if row['book_stat'] == 'receiving_yards':
+        return row['receiving_yards'] - row['pp_line']
+    elif row['book_stat'] == 'rushing_yards':
+        return row['rushing_yards'] - row['pp_line']
+    else:
+        return row['passing_yards'] - row['pp_line']
+    
+def ud_delta(row):
+    if row['book_stat'] == 'receiving_yards':
+        return row['receiving_yards'] - row['ud_line']
+    elif row['book_stat'] == 'rushing_yards':
+        return row['rushing_yards'] - row['ud_line']
+    else:
+        return row['passing_yards'] - row['ud_line']
+    
+# def player_selected(player_selected):
+#      player = nfl[(nfl.player_display_name==player_selected) & (nfl.season==2023)].reset_index(drop=True)
+#      return player
+
+# )
+
+# def get_rec_table_wide(player):
+#     player_df = all_data[(all_data.player_display_name==player) & (all_data.season==2023)].reset_index(drop=True) 
+#     rec_table_wide = player_df[['week','targets','receptions','receiving_tds','fantasy_points']].sort_values(by='week',ascending=False).reset_index(drop=True) 
+#     return rec_table_wide
+
+def get_rec_table_wide(player_season):
+    rec_table_wide = player_season[player_season.book_stat=='receiving_yards'][['week','targets','receptions','receiving_tds','fantasy_points']].sort_values(by='week',ascending=False).reset_index(drop=True) 
+    return rec_table_wide
+
+def get_rec_table_skinny(player_season):
+    rec_table_skinny = player_season[player_season.book_stat=='receiving_yards'][['week','receiving_yards']].sort_values(by='week',ascending=False).reset_index(drop=True) 
+    return rec_table_skinny
+
+def get_player_scatter_vertical(player_season):
+    player_scatter_vertical = px.scatter(player_season,x='targets',y='receiving_yards',
+                        size='week',color='week',template='presentation',
+                        size_max=17,width=500,height=700,
+                        color_continuous_scale='blues',
+                        # title = f"{player_season.player_display_name[0]}<br><b><span style='color:yellow'>{player_season.ud_line.mean()}</span><br><span style='color:purple'>{player_season.pp_line.mean()}</span><br>",
+                        # title = f"<span style='color:yellow'>ud <b>{player_season[player_season.book_stat=='receiving_yards'].ud_line.mean()}</b></span>   <span style='color:purple'>pp<b>{player_season[player_season.book_stat=='receiving_yards'].pp_line.mean()}</b></span>",                        
+                        labels={'receiving_yards':'Receiving Yards','targets':'Targets'}).update_coloraxes(showscale=False).add_hline(y=player_season[player_season['book_stat']=='receiving_yards'].ud_line.mean(), line_width=2, line_color="yellow").add_hline(y=player_season[player_season['book_stat']=='receiving_yards'].pp_line.mean(), line_width=2, line_color="purple").update_yaxes(showgrid=True, gridcolor='grey')
+    return player_scatter_vertical
+
+def get_player_scatter_horizontal(player_season2):
+    player_scatter_horizontal = px.scatter(player_season2,y='receiving_yards',
+                                           template='plotly_white', size='targets', size_max=13,
+                                           trendline="rolling", trendline_options=dict(function="median", window=6),trendline_scope="overall",
+                                           labels={'index':'<b>Game</b>', 'receiving_yards':'<b>Rec Yards</b>'},
+                                        #    width=1000,height=350,
+                                           title = f"{player_season2.player_display_name[0]}<br><b><span style='color:yellow'>{player_season2.ud_line.mean()}</span><br><span style='color:purple'>{player_season2.pp_line.mean()}</span><br>"
+                                           ).update_layout(showlegend=False).add_hline(y=(player_season2.pp_line.mean() + player_season2.ud_line.mean())/2, line_width=4, line_color="yellow")
+    return player_scatter_horizontal
