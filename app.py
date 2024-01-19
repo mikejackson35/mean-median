c1be5efd8ede1c20359ddb54531cbbd8a2ca645d
Mike_Jackson <mikejackson35@gmail.com>
2024-01-19 15:15:49 -0800

    update

--- app.py
+++ app.py
@@ -111,65 +111,37 @@ all_data = all_data.astype({'season': int})
 player_list = list(all_data.player_display_name.unique())#[::-1]
 player = st.selectbox('Select a player', player_list)
 player_all = all_data[(all_data.player_display_name==player)].reset_index(drop=True)
-st.markdown(f"<center><h1>{player_all.player_display_name[0]}</h1></center>", unsafe_allow_html=True)
+
+#####################
+#  TITLE
+st.markdown(f"<center><h1>{player_all.player_display_name[0]}</h1></center>", unsafe_allow_html=True)   
 
 player_season = player_all[player_all.season==2023].reset_index(drop=True)
 player_season2 = player_all[player_all.season > 2021].reset_index(drop=True)
 # st.markdown("")
 
-
-# col = st.columns(2)
-# with col[0]:
-#     st.markdown(f"<h1 style='color:yellow'><small>ud</small>{player_season[player_season.book_stat=='receiving_yards'].ud_line.mean()}</h1>",unsafe_allow_html=True)
-# with col[1]:
-#     st.markdown(f"<h1 style='color:purple'><small>pp</small>{player_season[player_season.book_stat=='receiving_yards'].pp_line.mean()}</h1>",unsafe_allow_html=True)
-
-# row1 = st.columns(2)
-# # row2 = st.columns(2)
-
-# for col in row1:
-#     tile = col.container()
-#     tile.markdown(f"<center><h1 style='color:yellow'><small>ud</small>{player_season[player_season.book_stat=='receiving_yards'].ud_line.mean()}</h1></center>",unsafe_allow_html=True)
-
-
+######################
+# PRIZE PICKS AND UNDERDOG LINES
 with st.container():
     col1,col2 = st.columns([2,1])
     with col1:
         st.markdown(f"<center><h1 style='color:yellow'><small>ud</small>{player_season[player_season.book_stat=='receiving_yards'].ud_line.mean()}</h1></center>",unsafe_allow_html=True)
     with col2:
         st.markdown(f"<center><h1 style='color:purple'><small>pp</small>{player_season[player_season.book_stat=='receiving_yards'].pp_line.mean()}</h1></center>",unsafe_allow_html=True)
-# line = (player_all.pp_line.mean() + player_all.ud_line.mean())/2
-# line2 = (player_all.pp_line.mean() + player_all.ud_line.mean())/3
-# st.markdown(f"<h5 style='color:yellow'>{line}</h5><h5 style='color:purple'>{round(line2)}</h5>",unsafe_allow_html=True)# {line2}")
-
-
-
-
-    
-# col = st.columns((1,9))
-
-# with col[0]:
-#         st.markdown("")
-# with col[1]:
-#         st.markdown(f"<left><h1>{player_all.player_display_name[0]}</h1></left>", unsafe_allow_html=True)
-#         col = st.columns(2)
-#         with col[0]:
-#              st.markdown(f"<left><h1 style='color:yellow'>ud{player_season[player_season.book_stat=='receiving_yards'].ud_line.mean()}</h1></left>",unsafe_allow_html=True)
-#         with col[1]:
-#              st.markdown(f"<left><h1 style='color:purple'>pp{player_season[player_season.book_stat=='receiving_yards'].pp_line.mean()}</h1></left>", unsafe_allow_html=True)
-
 
     
 #######################
-# Dashboard Main Panel
+# VERTICAL SCATTER
 col = st.columns([2,1])
 fig = get_player_scatter_vertical(player_season)
 config = {'displayModeBar': False}
 
-with col[0]:
-    st.plotly_chart(fig, config = config, theme=None,use_container_width=True)
-with col[1]:
-    st.dataframe(get_rec_table_skinny(player_season),hide_index=True, height=700,column_config={'week':'Week','receiving_yards':'Rec Yards'},use_container_width=True)
+with st.container(border=True):
+    col1,col2 = st.columns(2)
+    with col1:
+        st.plotly_chart(fig, config = config, theme=None,use_container_width=True)
+    with col2:
+        st.dataframe(get_rec_table_skinny(player_season),hide_index=True, height=700,column_config={'week':'Week','receiving_yards':'Rec Yards'},use_container_width=True)
 
 st.markdown("")
 st.dataframe(get_rec_table_wide(player_season),hide_index=True, height=700, column_config={'week':'Week','targets':'Targets','receptions':'Receptions','receiving_tds':'Receiving TDs','fantasy_points':'Fantasy Points'},use_container_width=True)
