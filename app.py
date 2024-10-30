66afe5e55a426946064086e20426e631fcb5fe47
Mike_Jackson <mikejackson35@gmail.com>
2024-10-30 12:07:20 -0700

    update

--- app.py
+++ app.py
@@ -1,159 +1,4 @@
-# #######################
-# # Import libraries
-# import streamlit as st
-# import altair as alt
-# import plotly.graph_objects as go
-# import pandas as pd
-# import numpy as np
-# import plotly.express as px
-# import seaborn as sns
-# import matplotlib.pyplot as plt
-# from utils import get_rush_table_wide, get_rec_table_wide, get_player_scatter_vertical,get_player_scatter_vertical_rush, get_player_scatter_vertical_pass, get_pass_table_wide
 
-# #######################
-# # Page configuration
-# st.set_page_config(
-#     page_title="mean-median",
-#     page_icon="🏂",
-#     layout="wide",
-#     initial_sidebar_state="expanded")
-
-# #######################
-# # CSS styling
-# st.markdown("""
-# <style>
-
-# [data-testid="block-container"] {
-#     padding-left: 1rem;
-#     padding-right: 1rem;
-#     padding-top: 2rem;
-#     padding-bottom: 0rem;
-#     max-width: 50%;
-#     margin: auto;
-# }
-
-# [data-testid="stVerticalBlock"] {
-#     padding-left: 0rem;
-#     padding-right: 0rem;
-# }
-
-# # [data-testid="stMetric"] {
-# #     background-color: #393939;
-# #     text-align: center;
-# #     padding: 15px 0;
-# # }
-
-# # [data-testid="stMetricLabel"] {
-# #   display: flex;
-# #   justify-content: center;
-# #   align-items: center;
-# # }
-
-# # [data-testid="stMetricDeltaIcon-Up"] {
-# #     position: relative;
-# #     left: 38%;
-# #     -webkit-transform: translateX(-50%);
-# #     -ms-transform: translateX(-50%);
-# #     transform: translateX(-50%);
-# # }
-
-# # [data-testid="stMetricDeltaIcon-Down"] {
-# #     position: relative;
-# #     left: 38%;
-# #     -webkit-transform: translateX(-50%);
-# #     -ms-transform: translateX(-50%);
-# #     transform: translateX(-50%);
-# # }
-
-# </style>
-# """, unsafe_allow_html=True)
-
-# #######################
-# # READ IN DATA
-# all_data = pd.read_csv(r"data/week_9.csv")
-# all_data = all_data.astype({'season': int})
-
-# st.markdown("<h1 style='text-align: center; font-family:Courier New;'>mean-median</h1>", unsafe_allow_html=True)
-# #####################
-# ## START TABS
-# tab1, tab2, tab3 = st.tabs(["Receiving", "Rushing", "Passing"])
-
-# with tab1:
-#     player_list = list(all_data[all_data.market=='receiving_yards'].player.sort_values().unique())
-#     player = st.selectbox(" ", player_list)
-#     player_season = all_data[all_data.player==player].reset_index(drop=True)
-
-#     col1,col2 = st.columns([1.35,1])
-
-#     with col1:
-#         st.plotly_chart(get_player_scatter_vertical(player_season), config = {'displayModeBar': False}, theme=None,use_container_width=True)
-#     with col2:
-#         st.markdown(" ")
-#         st.markdown(" ")
-#         st.markdown(" ")
-#         st.markdown(" ")
-#         st.markdown('<center>Game Log', unsafe_allow_html=True)
-
-#         st.dataframe(get_rec_table_wide(player_season),
-#                      hide_index=True, height=475,
-#                      column_config={
-#                           'Week': {'alignment': 'left', 'header': 'Week'},
-#                           'Yards': {'alignment': 'left', 'header': 'Yards'},
-#                           'Targets': {'alignment': 'left', 'header': 'Targets'},
-#                           'Rec': {'alignment': 'left', 'header': 'Rec'},
-#                           'TDs': {'alignment': 'left', 'header': 'TDs'}},
-#                           use_container_width=True)
-
-
-# with tab2:
-#     player_list = list(all_data[all_data.market=='rushing_yards'].player.sort_values().unique())
-#     player = st.selectbox(" ", player_list)
-#     player_season = all_data[all_data.player==player].reset_index(drop=True)
-
-#     col1,col2 = st.columns([1.5,1])
-
-#     with col1:
-#         st.plotly_chart(get_player_scatter_vertical_rush(player_season), config = {'displayModeBar': False}, theme=None,use_container_width=True)
-#     with col2:
-#         st.markdown(" ")
-#         st.markdown(" ")
-#         st.markdown(" ")
-#         st.markdown(" ")
-#         st.markdown('<center>Game Log', unsafe_allow_html=True)
-
-#         st.dataframe(get_rush_table_wide(player_season),
-#                      hide_index=True, height=475,
-#                      column_config={
-#                         'Week': {'alignment': 'left', 'header': 'Week'},
-#                         'Yards': {'alignment': 'left', 'header': 'Yards'},
-#                         'Carries': {'alignment': 'left', 'header': 'Carries'}},
-#                         use_container_width=True)
-
-
-# with tab3:
-#     player_list = list(all_data[all_data.market=='passing_yards'].player.sort_values().unique())
-#     player = st.selectbox(" ", player_list)
-#     player_season = all_data[all_data.player==player].reset_index(drop=True)
-
-#     col1,col2 = st.columns([1.18,1])
-
-#     with col1:
-#         st.plotly_chart(get_player_scatter_vertical_pass(player_season), config = {'displayModeBar': False}, theme=None,use_container_width=True)
-#     with col2:
-#         st.markdown(" ")
-#         st.markdown(" ")
-#         st.markdown(" ")
-#         st.markdown(" ")
-#         st.markdown('<center>Game Log', unsafe_allow_html=True)
-
-#         st.dataframe(get_pass_table_wide(player_season),
-#                      hide_index=True, height=475,
-#                      column_config={
-#                         'Week': {'alignment': 'left', 'header': 'Week'},
-#                         'Yards': {'alignment': 'left', 'header': 'Yards'},
-#                         'Attempts': {'alignment': 'left', 'header': 'Attempts'},
-#                         'TDs': {'alignment': 'left', 'header': 'TDs'}
-#                      },use_container_width=True)
 
 
 # ---- REMOVE UNWANTED STREAMLIT STYLING ----
@@ -314,3 +159,14 @@ with tab3:
                          'TDs': {'alignment': 'left', 'header': 'TDs'}
                      },
                      use_container_width=True)
+
+# ---- REMOVE UNWANTED STREAMLIT STYLING ----
+hide_st_style = """
+            <style>
+            Main Menu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+            
+st.markdown(hide_st_style, unsafe_allow_html=True)
