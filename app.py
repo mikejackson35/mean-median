d62a54665d29f7216ead7968e7bb58b90576585b
Mike_Jackson <mikejackson35@gmail.com>
2024-10-31 11:21:30 -0700

    update

--- app.py
+++ app.py
@@ -183,7 +183,7 @@ with tab1:
 
     with col2:
         '###'
-        '###'
+        # '###'
         st.markdown('<center>Game Log</center>', unsafe_allow_html=True)
         st.dataframe(
             get_rec_table_wide(player_season),
@@ -238,8 +238,6 @@ with tab2:
 
     with col2:
         '###'
-        '###'
-        # '###'
         # '###'
         st.markdown('<center>Game Log</center>', unsafe_allow_html=True)
         st.dataframe(
@@ -292,8 +290,6 @@ with tab3:
 
     with col2:
         '###'
-        '###'
-        # '###'
         # '###'
         st.markdown('<center>Game Log</center>', unsafe_allow_html=True)
         st.dataframe(
