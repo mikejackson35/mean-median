2f90abd00c3c63e60b1cef91be423f65777aec66
Mike_Jackson <mikejackson35@gmail.com>
2024-10-30 23:02:10 -0700

    upate

--- app.py
+++ app.py
@@ -146,73 +146,166 @@ with tab1:
             },
             use_container_width=True
         )
-
-
-
-
-
-
-
+        
 with tab2:
-    player_list = list(all_data[all_data.market=='rushing_yards'].player.sort_values().unique())
-    player = st.selectbox(" ", player_list)
-    player_season = all_data[all_data.player==player].reset_index(drop=True)
+    # Create the list of players and initialize the player season data
+    player_list = list(all_data[all_data.market == 'rushing_yards'].player.sort_values().unique())
+    player_season = all_data[all_data.player == player_list[0]].reset_index(drop=True)  # Initial player selection
 
-    col1, col2 = st.columns([1.5, 1])
+    col1, col2 = st.columns([1.35, 1])
 
     with col1:
-        st.markdown(f"<div style='text-align: center; color: white; font-size: 16px;'>{player_season.spread[0]} v. {player_season.opponent_team[0]}<br>o/u {player_season.over_under[0]}</div>", unsafe_allow_html=True)
-        st.plotly_chart(get_player_scatter_vertical_rush(player_season), config={'displayModeBar': False}, theme=None, use_container_width=True)
+        info_col1, info_col2, info_col3 = st.columns([1, 1.5, 1])
+
+        # Lines, player name, and game info
+        with info_col1:
+            lines_placeholder = st.empty()
+
+        with info_col2:
+            name_placeholder = st.empty()
+            game_placeholder = st.empty()
+
+        with info_col3:
+            lines_placeholder2 = st.empty()
+
+        # Display the scatter plot without the title
+        scatter_placeholder = st.empty()
+
+        # Centered selectbox with reduced width
+        center_col1, selectbox_col, center_col2 = st.columns([1, 6, 1])  # Adjust widths as needed
+
+        with selectbox_col:
+            # Make selectbox and update player
+            player = st.selectbox("Select Player", player_list, key='tab2_select')
+            player_season = all_data[all_data.player == player].reset_index(drop=True)
+
+            # Display underdog and prizepicks lines
+            lines_placeholder.markdown(f"<div style='text-align: center; color: yellow; font-size: 20px;'>"
+                                       f"Udog<br>{player_season[player_season.market == 'rushing_yards'].fillna(0).ud_line.median()}<br>",
+                                       unsafe_allow_html=True)
+
+            lines_placeholder2.markdown(f"<div style='text-align: center; color: yellow; font-size: 20px;'>"
+                                        f"<span style='color: violet;'>Ppick<br>{player_season[player_season.market == 'rushing_yards'].fillna(0).pp_line.median()}</span></div>",
+                                        unsafe_allow_html=True)
+
+            # Display player name
+            name_placeholder.markdown(f"<div style='text-align: center; color: white; font-size: 24px;'>"
+                                      f"<b>{player_season.player[0]}</b></div>",
+                                      unsafe_allow_html=True)
+
+            # Conditional logic to add a '+' symbol if the spread is non-negative
+            spread_display = f"+{player_season.spread[0]}" if player_season.spread[0] >= 0 else str(player_season.spread[0])
+
+            # Using <span> to add more space instead of a pipe
+            game_placeholder.markdown(f"<div style='text-align: center; color: white; font-size: 16px;'>"
+                                      f"<b><small>{spread_display} v. {player_season.opponent_team[0]} <span style='margin-left: 20px;'>o/u</span> {(player_season.over_under[0])}</small></b></div>",
+                                      unsafe_allow_html=True)
+
+            scatter_placeholder.plotly_chart(get_player_scatter_vertical_rush(player_season), config={'displayModeBar': False}, theme=None, use_container_width=True)
+
     with col2:
-        st.markdown(" ")
-        st.markdown(" ")
-        st.markdown(" ")
-        st.markdown(" ")
-        st.markdown('<center>Game Log', unsafe_allow_html=True)
-
-        st.dataframe(get_rush_table_wide(player_season),
-                     hide_index=True, height=475,
-                     column_config={
-                         'Week': {'alignment': 'left', 'header': 'Week'},
-                         'Yards': {'alignment': 'left', 'header': 'Yards'},
-                         'Carries': {'alignment': 'left', 'header': 'Carries'}
-                     },
-                     use_container_width=True)
-
+        '###'
+        '###'
+        '###'
+        '###'
+        st.markdown('<center>Game Log</center>', unsafe_allow_html=True)
+        st.dataframe(
+            get_rush_table_wide(player_season),
+            hide_index=True,
+            height=475,
+            column_config={
+                'Week': {'alignment': 'left', 'header': 'Week'},
+                'Yards': {'alignment': 'left', 'header': 'Yards'},
+                'Carries': {'alignment': 'left', 'header': 'Carries'}
+            },
+            use_container_width=True
+        )
+     
+# Tab 3: Passing Yards Data
 with tab3:
-    player_list = list(all_data[all_data.market=='passing_yards'].player.sort_values().unique())
-    player = st.selectbox(" ", player_list)
-    player_season = all_data[all_data.player==player].reset_index(drop=True)
+    # Create the list of players and initialize the player season data
+    player_list = list(all_data[all_data.market == 'passing_yards'].player.sort_values().unique())
+    player_season = all_data[all_data.player == player_list[0]].reset_index(drop=True)  # Initial player selection
 
-    col1, col2 = st.columns([1.18, 1])
+    col1, col2 = st.columns([1.35, 1])
 
     with col1:
-        st.markdown(f"<div style='text-align: center; color: white; font-size: 16px;'>{player_season.spread[0]} v. {player_season.opponent_team[0]}<br>o/u {player_season.over_under[0]}</div>", unsafe_allow_html=True)
-        st.plotly_chart(get_player_scatter_vertical_pass(player_season), config={'displayModeBar': False}, theme=None, use_container_width=True)
-    with col2:
-        st.markdown(" ")
-        st.markdown(" ")
-        st.markdown(" ")
-        st.markdown(" ")
-        st.markdown('<center>Game Log', unsafe_allow_html=True)
+        info_col1, info_col2, info_col3 = st.columns([1, 1.5, 1])
 
-        st.dataframe(get_pass_table_wide(player_season),
-                     hide_index=True, height=475,
-                     column_config={
-                         'Week': {'alignment': 'left', 'header': 'Week'},
-                         'Yards': {'alignment': 'left', 'header': 'Yards'},
-                         'Attempts': {'alignment': 'left', 'header': 'Attempts'},
-                         'TDs': {'alignment': 'left', 'header': 'TDs'}
-                     },
-                     use_container_width=True)
+        # Lines, player name, and game info
+        with info_col1:
+            lines_placeholder = st.empty()
+
+        with info_col2:
+            name_placeholder = st.empty()
+            game_placeholder = st.empty()
+
+        with info_col3:
+            lines_placeholder2 = st.empty()
+
+        # Display the scatter plot without the title
+        scatter_placeholder = st.empty()
+
+        # Centered selectbox with reduced width
+        center_col1, selectbox_col, center_col2 = st.columns([1, 6, 1])  # Adjust widths as needed
+
+        with selectbox_col:
+            # Make selectbox and update player
+            player = st.selectbox("Select Player", player_list, key='tab3_select')
+            player_season = all_data[all_data.player == player].reset_index(drop=True)
+
+            # Display underdog and prizepicks lines
+            lines_placeholder.markdown(f"<div style='text-align: center; color: yellow; font-size: 20px;'>"
+                                       f"Udog<br>{player_season[player_season.market == 'passing_yards'].fillna(0).ud_line.median()}<br>",
+                                       unsafe_allow_html=True)
+
+            lines_placeholder2.markdown(f"<div style='text-align: center; color: yellow; font-size: 20px;'>"
+                                        f"<span style='color: violet;'>Ppick<br>{player_season[player_season.market == 'passing_yards'].fillna(0).pp_line.median()}</span></div>",
+                                        unsafe_allow_html=True)
+
+            # Display player name
+            name_placeholder.markdown(f"<div style='text-align: center; color: white; font-size: 24px;'>"
+                                      f"<b>{player_season.player[0]}</b></div>",
+                                      unsafe_allow_html=True)
+
+            # Conditional logic to add a '+' symbol if the spread is non-negative
+            spread_display = f"+{player_season.spread[0]}" if player_season.spread[0] >= 0 else str(player_season.spread[0])
+
+            # Using <span> to add more space instead of a pipe
+            game_placeholder.markdown(f"<div style='text-align: center; color: white; font-size: 16px;'>"
+                                      f"<b><small>{spread_display} v. {player_season.opponent_team[0]} <span style='margin-left: 20px;'>o/u</span> {(player_season.over_under[0])}</small></b></div>",
+                                      unsafe_allow_html=True)
+
+            scatter_placeholder.plotly_chart(get_player_scatter_vertical_pass(player_season), config={'displayModeBar': False}, theme=None, use_container_width=True)
+
+    with col2:
+        '###'
+        '###'
+        '###'
+        '###'
+        st.markdown('<center>Game Log</center>', unsafe_allow_html=True)
+        st.dataframe(
+            get_pass_table_wide(player_season),
+            hide_index=True,
+            height=475,
+            column_config={
+                'Week': {'alignment': 'left', 'header': 'Week'},
+                'Yards': {'alignment': 'left', 'header': 'Yards'},
+                'Attempts': {'alignment': 'left', 'header': 'Attempts'},
+                'TDs': {'alignment': 'left', 'header': 'TDs'}
+            },
+            use_container_width=True
+        )
+
+
 
 # ---- REMOVE UNWANTED STREAMLIT STYLING ----
-# hide_st_style = """
-#             <style>
-#             Main Menu {visibility: hidden;}
-#             footer {visibility: hidden;}
-#             header {visibility: hidden;}
-#             </style>
-#             """
+hide_st_style = """
+            <style>
+            Main Menu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
             
-# st.markdown(hide_st_style, unsafe_allow_html=True)
+st.markdown(hide_st_style, unsafe_allow_html=True)
--- utils.py
+++ utils.py
@@ -52,42 +52,43 @@ def get_rec_table_wide(player_season):
     
     return rec_table_wide
 
-
 def get_rush_table_wide(player_season):
-    rush_table_wide = player_season[player_season.market=='rushing_yards'][['week','rushing_yards','carries']].sort_values(by='week',ascending=False).reset_index(drop=True)
-    rush_table_wide.columns = ['Week','Yards','Carries']
+    # Create a DataFrame for rushing yards data, sorting by week
+    rush_table_wide = player_season[player_season.market == 'rushing_yards'][['week', 'opponent_team', 'rushing_yards', 'carries']].sort_values(by='week', ascending=False).reset_index(drop=True)
+    rush_table_wide.columns = ['Week', 'Opp', 'Yards', 'Carries']
 
-    ud_line = player_season[player_season.market=='rushing_yards'].ud_line.median()
-    pp_line = player_season[player_season.market=='rushing_yards'].pp_line.median()
+    # Calculate the median lines for UD and PP
+    ud_line = player_season[player_season.market == 'rushing_yards'].ud_line.median()
+    pp_line = player_season[player_season.market == 'rushing_yards'].pp_line.median()
 
-    # Define a function to highlight rows where 'receiving_yards' is greater than either ud_line or pp_line
+    # Define a function to highlight rows where 'Yards' is greater than either ud_line or pp_line
     def highlight_high_yards(row):
         color = 'background-color: #65b1d7' if row['Yards'] > ud_line or row['Yards'] > pp_line else ''
         return [color] * len(row)
-    
-    # Apply the highlighting function and set font properties
+
+    # Apply the highlighting function and format the DataFrame
     rush_table_wide = (rush_table_wide.style
-                      .apply(highlight_high_yards, axis=1)
-                      .format(precision=0))
-    
+                       .apply(highlight_high_yards, axis=1)
+                       .format(precision=0))
+
     return rush_table_wide
 
 def get_pass_table_wide(player_season):
-    pass_table_wide = player_season[player_season.market=='passing_yards'][['week','passing_yards','attempts','passing_tds']].sort_values(by='week',ascending=False).reset_index(drop=True) 
-    pass_table_wide.columns = ['Week','Yards','Attempts','TDs']
+    pass_table_wide = player_season[player_season.market == 'passing_yards'][['week', 'passing_yards', 'attempts', 'passing_tds']].sort_values(by='week', ascending=False).reset_index(drop=True)
+    pass_table_wide.columns = ['Week', 'Yards', 'Attempts', 'TDs']
 
-    ud_line = player_season[player_season.market=='passing_yards'].ud_line.median()
-    pp_line = player_season[player_season.market=='passing_yards'].pp_line.median()
+    ud_line = player_season[player_season.market == 'passing_yards'].ud_line.median()
+    pp_line = player_season[player_season.market == 'passing_yards'].pp_line.median()
 
-    # function to hightlight weeks beating line
+    # Define a function to highlight rows where 'passing_yards' is greater than either ud_line or pp_line
     def highlight_high_yards(row):
         color = 'background-color: #65b1d7' if row['Yards'] > ud_line or row['Yards'] > pp_line else ''
         return [color] * len(row)
     
     # Apply the highlighting function and set font properties
     pass_table_wide = (pass_table_wide.style
-                      .apply(highlight_high_yards, axis=1)
-                      .format(precision=0))
+                       .apply(highlight_high_yards, axis=1)
+                       .format(precision=0))
     
     return pass_table_wide
 
@@ -96,19 +97,48 @@ def get_player_scatter_vertical(player_season):
     # Create a new column for custom hover text
     player_season['hover_text'] = player_season.apply(
         lambda row: f"Week {int(row['week'])}<br>vs. {row['opponent_team']}<br><br>{int(row['receiving_yards'])} yards<br>{int(row['targets'])} targets", axis=1
-        )
+    )
     
-    player_scatter_vertical = px.scatter(player_season,x='targets',y='receiving_yards',
-                        size='week',color='week',template='presentation',
-                        size_max=17, height=550, #width=500
-                        color_continuous_scale='blues',
-                        # title = f"<b>{player_season.player[0]}<br><span style='color:yellow'>{player_season[player_season.market == 'receiving_yards'].fillna(0).ud_line.median()}</span> <span style='color:orchid'><b>{player_season[player_season.market == 'receiving_yards'].fillna(0).pp_line.median()}</span>",
-                        labels={'receiving_yards':'Receiving Yards','targets':'Targets'}).update_coloraxes(showscale=False)
-    player_scatter_vertical.add_hline(y=player_season[player_season['market']=='receiving_yards'].ud_line.max(), line_width=1, line_color="yellow")
-    player_scatter_vertical.add_hline(y=player_season[player_season['market']=='receiving_yards'].pp_line.max(), line_width=1, line_color="purple")
-    player_scatter_vertical.add_hline(y=player_season[player_season['market']=='receiving_yards'].receiving_yards.median(), line_width=2, line_color="white", line_dash="dot")
+    player_scatter_vertical = px.scatter(
+        player_season,
+        x='targets',
+        y='receiving_yards',
+        size='week',
+        color='week',
+        template='presentation',
+        size_max=17,
+        height=550,
+        color_continuous_scale='blues',
+        labels={'receiving_yards': 'Receiving Yards', 'targets': 'Targets'}
+    ).update_coloraxes(showscale=False)
+
+    # Calculate the maximum values for UD Line and PP Line
+    ud_line_max = player_season[player_season['market'] == 'receiving_yards'].ud_line.max()
+    pp_line_max = player_season[player_season['market'] == 'receiving_yards'].pp_line.max()
+
+    # Add horizontal lines only if the values are greater than zero
+    if ud_line_max > 0:
+        player_scatter_vertical.add_hline(y=ud_line_max, line_width=1, line_color="yellow")
+    
+    if pp_line_max > 0:
+        player_scatter_vertical.add_hline(y=pp_line_max, line_width=1, line_color="purple")
+    
+    # Median line and annotation
+    median_value = player_season[player_season['market'] == 'receiving_yards'].receiving_yards.median()
+    player_scatter_vertical.add_hline(y=median_value, line_width=3, line_color="white", line_dash="dot")
+    
+    # Add annotation for the median line
+    player_scatter_vertical.add_annotation(
+        x=.5,  # Positioning on the x-axis (far left)
+        y=median_value + 2,  # Slightly above the line
+        text=f'<b>Med {int(median_value)}',  # Value to display
+        showarrow=False,  # No arrow
+        font=dict(size=15, color='white'),  # Font size and color
+        borderpad=3,  # Padding of the border
+        align='center'  # Center the text
+    )
+    
     player_scatter_vertical.update_yaxes(showgrid=True, gridcolor='grey')
-    player_scatter_vertical.update_layout(title_y=.9)
     player_scatter_vertical.update_traces(textfont_family='Arial Black', hovertemplate=player_season['hover_text'])
 
     return player_scatter_vertical
@@ -117,20 +147,49 @@ def get_player_scatter_vertical_rush(player_season):
     # Create a new column for custom hover text
     player_season['hover_text'] = player_season.apply(
         lambda row: f"Week {int(row['week'])}<br>vs. {row['opponent_team']}<br><br>{int(row['rushing_yards'])} yards<br>{int(row['carries'])} carries", axis=1
-        )
+    )
     
-    player_scatter_vertical = px.scatter(player_season,x='carries',y='rushing_yards',
-                        size='week',color='week',template='presentation',
-                        size_max=17, height=550, #width=500
-                        color_continuous_scale='blues',
-                        title = f"<b>{player_season.player[0]}<br><span style='color:yellow'>{player_season[player_season.market == 'rushing_yards'].fillna(0).ud_line.median()}</span> <span style='color:orchid'><b>{player_season[player_season.market == 'rushing_yards'].fillna(0).pp_line.median()}</span><br>",
-                        labels={'rushing_yards':'Rush Yards','carries':'Carries'}).update_coloraxes(showscale=False)
-    player_scatter_vertical.add_hline(y=player_season[player_season['market']=='rushing_yards'].fillna(0).ud_line.median(), line_width=1, line_color="yellow")
-    player_scatter_vertical.add_hline(y=player_season[player_season['market']=='rushing_yards'].fillna(0).pp_line.median(), line_width=1, line_color="purple")
-    player_scatter_vertical.add_hline(y=player_season[player_season['market']=='rushing_yards'].fillna(0).rushing_yards.median(), line_width=2, line_color="white", line_dash="dot")
-    player_scatter_vertical.update_yaxes(showgrid=True, gridcolor='grey')
-    player_scatter_vertical.update_layout(title_y=.9)
-    player_scatter_vertical.update_traces(textfont_family='Arial Black', hovertemplate=player_season['hover_text'])
+    # Create the scatter plot for rushing yards
+    player_scatter_vertical = px.scatter(
+        player_season,
+        x='carries',
+        y='rushing_yards',
+        size='week',
+        color='week',
+        template='presentation',
+        size_max=17,
+        height=550,
+        color_continuous_scale='blues',
+        labels={'rushing_yards': 'Rush Yards', 'carries': 'Carries'}
+    ).update_coloraxes(showscale=False)
+
+    # Calculate the maximum values for UD Line and PP Line
+    ud_line_max = player_season[player_season['market'] == 'rushing_yards'].ud_line.max()
+    pp_line_max = player_season[player_season['market'] == 'rushing_yards'].pp_line.max()
+
+    # Add horizontal lines only if the values are greater than zero
+    if ud_line_max > 0:
+        player_scatter_vertical.add_hline(y=ud_line_max, line_width=1, line_color="yellow")
+    
+    if pp_line_max > 0:
+        player_scatter_vertical.add_hline(y=pp_line_max, line_width=1, line_color="purple")
+    
+    # Median line and annotation
+    median_value = player_season[player_season['market'] == 'rushing_yards'].rushing_yards.median()
+    player_scatter_vertical.add_hline(y=median_value, line_width=3, line_color="white", line_dash="dot")
+    
+    # Add annotation for the median line
+    player_scatter_vertical.add_annotation(
+        x=.5,  # Positioning on the x-axis (far left)
+        y=median_value + 2,  # Slightly above the line
+        text=f'<b>Med {int(median_value)}',  # Value to display
+        showarrow=False,  # No arrow
+        font=dict(size=15, color='white'),  # Font size and color
+        align='center'  # Center the text
+    )
+    
+    # Update layout and axes
+    player_scatter_vertical.update_traces(hovertemplate=player_season['hover_text'])
 
     return player_scatter_vertical
 
@@ -138,17 +197,47 @@ def get_player_scatter_vertical_pass(player_season):
     # Create a new column for custom hover text
     player_season['hover_text'] = player_season.apply(
         lambda row: f"Week {int(row['week'])}<br>vs. {row['opponent_team']}<br><br>{int(row['passing_yards'])} yards<br>{int(row['attempts'])} attempts", axis=1
-        )
-    player_scatter_vertical = px.scatter(player_season,x='attempts',y='passing_yards',
-                        size='week',color='week',template='presentation',
-                        size_max=17, height=550, #width=500
-                        color_continuous_scale='blues',
-                        title = f"<b>{player_season.player[0]}<br><span style='color:yellow'>{player_season[player_season.market == 'passing_yards'].fillna(0).ud_line.median()}</span> <span style='color:orchid'><b>{player_season[player_season.market == 'passing_yards'].fillna(0).pp_line.median()}</span><br>",
-                        labels={'passing_yards':'Pass Yards','attempts':'Attempts'}).update_coloraxes(showscale=False)
-    player_scatter_vertical.add_hline(y=player_season[player_season['market']=='passing_yards'].ud_line.mean(), line_width=1, line_color="yellow")
-    player_scatter_vertical.add_hline(y=player_season[player_season['market']=='passing_yards'].pp_line.mean(), line_width=1, line_color="lavender")
-    player_scatter_vertical.add_hline(y=player_season[player_season['market']=='passing_yards'].passing_yards.median(), line_width=2, line_color="white", line_dash="dot")
-    player_scatter_vertical.update_yaxes(showgrid=True, gridcolor='grey')
-    player_scatter_vertical.update_layout(title_y=.9)
-    player_scatter_vertical.update_traces(textfont_family='Arial Black', hovertemplate=player_season['hover_text'])
+    )
+    
+    # Create the scatter plot for passing yards
+    player_scatter_vertical = px.scatter(
+        player_season,
+        x='attempts',
+        y='passing_yards',
+        size='week',
+        color='week',
+        template='presentation',
+        size_max=17,
+        height=550,
+        color_continuous_scale='blues',
+    ).update_coloraxes(showscale=False)
+
+    # Calculate the maximum values for UD Line and PP Line
+    ud_line_max = player_season[player_season['market'] == 'passing_yards'].ud_line.max()
+    pp_line_max = player_season[player_season['market'] == 'passing_yards'].pp_line.max()
+
+    # Add horizontal lines only if the values are greater than zero
+    if ud_line_max > 0:
+        player_scatter_vertical.add_hline(y=ud_line_max, line_width=1, line_color="yellow")
+    
+    if pp_line_max > 0:
+        player_scatter_vertical.add_hline(y=pp_line_max, line_width=1, line_color="purple")
+    
+    # Median line and annotation
+    median_value = player_season[player_season['market'] == 'passing_yards'].passing_yards.median()
+    player_scatter_vertical.add_hline(y=median_value, line_width=3, line_color="white", line_dash="dot")
+    
+    # Add annotation for the median line
+    player_scatter_vertical.add_annotation(
+        x=.5,  # Positioning on the x-axis (far left)
+        y=median_value + 5,  # Slightly above the line
+        text=f'<b>Med {int(median_value)}',  # Value to display
+        showarrow=False,  # No arrow
+        font=dict(size=15, color='white'),  # Font size and color
+        align='center'  # Center the text
+    )
+    
+    # Update layout and axes
+    player_scatter_vertical.update_traces(hovertemplate=player_season['hover_text'])
+
     return player_scatter_vertical
